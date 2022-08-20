7a312214ac2c2c51f6504883adc17c268b983d14
Jonathan Alonso <JA1@users.noreply.github.com>
2022-08-20 11:25:40 -0400

    t

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+function listen() {
+    let el = document.getElementById('centerBanner')
+    el.addEventListener('keydown', (e) => {
+        if (e.keyCode === 13) {
+            search(query.value)
+            removeEventListener('keydown', (e) => {
+                search(query.value)
+                el.removeEventListener('keydown')
+            })
+        }
+    })
+}
+
 function search(query) {
     alert('You have searched for ' + query);
 }
-
-document.getElementById('searchTerm').addEventListener('keypress', (e)=> {
-    if (e.key === 'Enter') {
-        search(query.value);
-    }
-})
