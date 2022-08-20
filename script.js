2d280e2dbe2482773a23de995f5889b6abf5a006
Jonathan Alonso <JA1@users.noreply.github.com>
2022-08-20 09:25:18 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function search(query) {
-    alert('You have searched for ' + query)
+    alert('You have searched for ' + query);
 }
 
 document.getElementById('searchTerm').addEventListener('keypress', (e)=> {
     if (e.key === 'Enter') {
-        search(query.value)
+        search(query.value);
     }
 })
